073ab6f0b180f48fa03e6b41bfef30090e755a39
Mariia Fedorova <000maria.fed@gmail.com>
2023-06-02 21:19:37 +0300

    добавление функции перевода первых букв в верхний регистр

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def func(text):
-"""функция перевода текста в верхний регистр"""
+
+
+    "функция перевода текста в верхний регистр"
     return text.upper()
+def title(text):
+    return text.title()
